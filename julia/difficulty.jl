8c71c801be0ac8f28be95e04afb2e03f9e4e3886
Aaron Peikert <aaron.peikert@posteo.de>
2022-05-10 18:31:41 +0200

    buitify plots

--- julia/difficulty.jl
+++ julia/difficulty.jl
@@ -3,11 +3,11 @@ using Plots
 x = 1:7
 spss = [1, 2, 3, 10, 10, 10, 10]
 logocolors = Colors.JULIA_LOGO_COLORS
-plot(x, spss, label = "SPSS", legend=:bottomright, xguidefontsize=10, xticks = nothing, yticks = nothing, margin=5Plots.mm, background_color = RGB(1,1,243/255), color = logocolors.red)
+plot(x, spss, label = "SPSS", legend=:bottomright, xguidefontsize=10, xticks = nothing, yticks = nothing, linewidth = 2, margin=10Plots.mm, background_color = RGB(1,1,243/255), color = logocolors.red)
 r = [2, 2.5, 3, 3.5, 4, 6, 10]
-plot!(x, r, label = "R", color = logocolors.blue)
+plot!(x, r, label = "R", color = logocolors.blue, linewidth = 2)
 julia = 2.5:.5:5.5
-plot!(x, julia, label = "Julia", color = logocolors.green)
+plot!(x, julia, label = "Julia", color = logocolors.green, linewidth = 2)
 xlabel!("Personal use ⟶ Publication ⟶ Reproducible ⟶ Reusable ⟶ Extensible")
 ylabel!("Hobby ⟶ Part time ⟶ Full time")
 
--- julia/users.jl
+++ julia/users.jl
@@ -2,7 +2,7 @@ println(Base.active_project())
 using Plots
 x = 1:7
 logocolors = Colors.JULIA_LOGO_COLORS
-plot(x, [1, 2, 5, 30, 50, 150, 300], label = nothing, legend=:bottomright, xguidefontsize=10, yticks = nothing, xticks = nothing, margin=5Plots.mm, background_color = RGB(1,1,243/255), color = logocolors.purple)
+plot(x, [1, 2, 5, 30, 50, 150, 300], label = nothing, legend=:bottomright, xguidefontsize=10, yticks = nothing, linewidth = 2, xticks = nothing, margin=10Plots.mm, background_color = RGB(1,1,243/255), color = logocolors.purple)
 xlabel!("Personal use ⟶ Publication ⟶ Reproducible ⟶ Reusable ⟶ Extensible")
 ylabel!("Nr. Users of Code")
 savefig("figures/users.pdf")
