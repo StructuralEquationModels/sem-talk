7065c9668a766e803e3d437f374c6afc93cf7ac3
Aaron Peikert <aaron.peikert@posteo.de>
2022-05-11 09:06:49 +0200

    change graph

--- julia/users.jl
+++ julia/users.jl
@@ -4,5 +4,5 @@ x = 1:7
 logocolors = Colors.JULIA_LOGO_COLORS
 plot(x, [1, 2, 5, 30, 50, 150, 300], label = nothing, legend=:bottomright, xguidefontsize=10, yticks = nothing, linewidth = 2, xticks = nothing, margin=10Plots.mm, background_color = RGB(1,1,243/255), color = logocolors.purple)
 xlabel!("Personal use ⟶ Publication ⟶ Reproducible ⟶ Reusable ⟶ Extensible")
-ylabel!("Nr. Users of Code")
+ylabel!("Nr. Users of a Piece of Code")
 savefig("figures/users.pdf")
